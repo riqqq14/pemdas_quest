bf186d1394b48bbdeea481767674eb0cd510c771
Thoriq14 <110508884+riqqq14@users.noreply.github.com>
2022-08-07 18:51:15 -0700

    Add files via upload

---
+++ python1 .py
@@ -0,0 +1,12 @@
+# Belajar Output
+
+print("Hello world") 
+print("Nama Saya Thoriq")
+print("Saya penyuka kucing")
+print("Saya wangi")
+print("Hobi bermain bola")
+print("Fav lagu december")
+print("Makanan kesukaan nasi goreng")
+print("Minuman kesukaan matcha")
+print("#stop ngelem")
+print("Cita-cita pemain bola")
---
+++ python2 .py
@@ -0,0 +1,12 @@
+#Belajar Input
+
+Nama = "thoriq" # nama adalah Data, Thoriq adalah variabel 
+umur = 15 # Tipe data integer
+alamat = "beji, kota Depok"
+
+print(Nama)
+print(umur)
+print(alamat)
+
+print("Nama saya adalah " + Nama )
+#print("Nama saya adalah " + Nama + " umur saya " ) + str (umur) + " Alamat saya " + alamat)
