bde969d546cc94628502770b839d1fe9f8532b80
Thoriq14 <110508884+riqqq14@users.noreply.github.com>
2022-08-10 19:08:39 -0700

    Add files via upload

---
+++ python3 .py
@@ -0,0 +1,8 @@
+#Belajar input 2
+
+#pertama kita belajar untuk input data string
+
+nama = input("Masukkan Nama anda:") #INPUT
+nama_depan = input("Masukan nama depan anda adalah: ") #INPUT
+print("Nama anda adalah", nama) #OUTPUT
+print("Nama depan anda adalah", nama_depan) #OUTPUT
---
+++ python4 .py
@@ -0,0 +1,29 @@
+#Belajar INPUT 3
+
+#Belajar Matematika dalam program
+
+a = 5
+b = 10
+c = a + b 
+
+print("a", a)
+print("b", b)
+print("c", c)
+
+
+x = int(input("masukkan Angka ke-1"))
+y = int(input("masukkan Angka Ke-2"))
+
+print("Nilai x=" ,x)
+print("Nilai=",y)
+print("x + y = ", x+y)
+print("x - y = ", x-y)
+print("x * y = ", x*y)
+print("x / y = ", x/y)
+
+suhu = int(input("masukkan suhu celcius:"))
+fah = 0 #tentukan rumus celcius ke farenheit
+print(suhu, "celcius=", fah, "farenheit")
+suhu = int(input("masukkan suhu celcius:"))
+fah = (9/5 * suhu) + 32
+print(suhu, "celcius =", fah, "farenheit")
